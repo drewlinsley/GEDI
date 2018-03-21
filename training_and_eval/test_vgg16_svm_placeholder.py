b0bba1c64a272898d702dcab83e37b4cd0262922
Drew Linsley <drewlinsley@gmail.com>
2018-03-21 16:12:36 -0400

    Fixed test svm.

--- training_and_eval/test_vgg16_svm_placeholder.py
+++ training_and_eval/test_vgg16_svm_placeholder.py
@@ -264,9 +264,9 @@ def test_vgg16(
         p_value=p_value)
 
     # Also save a csv with item/guess pairs
-    trimmed_files = [re.split('/', x)[-1] for x in ckpt_file_array]
-    trimmed_files = np.asarray(trimmed_files)
-    dec_scores = np.asarray(dec_scores)
+    ckpt_file_array = ckpt_file_array.ravel()
+    trimmed_files = np.asarray(
+        [x.split(os.path.sep)[-1] for x in ckpt_file_array.ravel()])
     yhat = np.asarray(yhat)
     df = pd.DataFrame(
         np.hstack((
